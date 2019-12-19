21f65c7b5eaf27a2bdf1a1524b177e50a6508f5a
Ashika Vijay <ashika.vijay@malinkoapp.com>
2019-12-19 11:17:38 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = 'f4b3fedeceeabc360134dee6fbeedc42d4848b875eea4241b978f0c243fe94d960c1d6db3cb1945d94a5917da5e34acf58bbc261f277201c5223c2cfa06908ae'
