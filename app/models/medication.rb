c7e0ef96ad2dae239ec4ca5ee79fdfb502ac81e6
Ashika Vijay <ashika.vijay@malinkoapp.com>
2020-01-13 10:26:30 +0000

    Fixed all indentations

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,13 +1,5 @@
 class Medication < ApplicationRecord
 
-	# attr_accessor(
- #    :name,
- #    :start_date,
- #    :end_date,
- #    :dose,
- #    :notes
- #  )
-
 	  validate :name
   	validate :start_date
  	  validate :end_date
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'summary/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- resources :medications
+  resources :medications
  
- root 'summary#index'
+  root 'summary#index'
 end
 
